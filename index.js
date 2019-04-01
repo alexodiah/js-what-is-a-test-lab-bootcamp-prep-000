1a8ec9046ea5a449f20fc281a2f7b0441e44a180
Emeka, Alex Odiah <alexodiah@gmail.com>
2019-04-01 12:44:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ var name = "Susan"
 var height = 74;
 var message = `${name} is ${height} inches tall`
     console.log((height)-32)
-    console.
+    console.log(message)
 module.exports = {name, height, message}
